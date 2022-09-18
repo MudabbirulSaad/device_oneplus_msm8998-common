05af9c756b6201819252ce1a956e83d21a36f2a5
hpnightowl <hpnightowl@gmail.com>
2022-09-18 11:58:49 +0600

    msm8998-common: drop audio.a2dp.default

--- common.mk
+++ common.mk
@@ -90,7 +90,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
-    audio.a2dp.default \
     audio.primary.msm8998 \
     audio.r_submix.default \
     audio.usb.default \
