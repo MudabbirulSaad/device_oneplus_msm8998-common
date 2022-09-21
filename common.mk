c4851b10c0f79e27e351510fb817dfd9df333cf2
Mudabbirul Saad <mudabbirulsaad@gmail.com>
2022-09-21 11:54:34 +0600

    OnePlusPocketMode: Temporary removed for now.

--- common.mk
+++ common.mk
@@ -349,10 +349,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
-# Pocket mode
-PRODUCT_PACKAGES += \
-    OnePlusPocketMode
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.oneplus_msm8998-libperfmgr
